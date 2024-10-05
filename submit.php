1a77ac9fe7043b7aaa44cb860aad831630dfbca2
Phillipmutuse <phillip.mwengi@strathmore.edu>
2024-10-05 16:35:05 +0300

    33

--- submit.php
+++ submit.php
@@ -5,4 +5,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
   $password = $_POST['password'];
 
- 
+  if (empty($username) || !$email || empty($password)) {
+    die("Please fill in all fields and provide a valid email.");
+  }
+
+  // Password hashing for security
+  $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+
+  // Continue with storing data to the database
+}
+?>
