256d315bb7f4cb3ff3729c4dfa24f151f61ce219
Phillipmutuse <phillip.mwengi@strathmore.edu>
2024-10-05 22:27:08 +0300

    15

--- db.sql
+++ db.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE lab_oop_php;
 
+USE lab_oop_php;
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
